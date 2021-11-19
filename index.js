3d65ecdfab1d2e8c9c2716cd12aed7baf80526ee
Michael Pendleton <Pendletonm@gmail.com>
2021-11-19 14:33:14 -0500

    changed the language around selecting a game to be clearer, added cheat detecter to H or L input in game one

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function ask(questionText) {
   });
 }
 
-//asks player what game they would like to play 
+//asks player what game they would like to play
 selectGameType();
 
 async function numberGame() {
   console.log(
-    "Let's play a game where you (human) make up a number and I (computer) try to guess it.\nBut first I need you to tell me the numbers you are choosing between, and that I will be guessing between..."
+    "Ok! You will pick a number and I will try to guess it\nBut first I need you to tell me the numbers you are choosing between, and that I will guessing between..."
   );
   //define the minimum range for the game we are playing
   let min = await ask(
@@ -31,9 +31,9 @@ async function numberGame() {
   console.log("You entered: " + secretNumber);
   secretNumber = parseInt(secretNumber);
 
-  //intention was to contain the entire game within this loop, within the function makeGuess are other nested functions to help it do its job. 
+  //intention was to contain the entire game within this loop, within the function makeGuess are other nested functions to help it do its job.
   while (true) {
-    await makeGuess(min, max,);
+    await makeGuess(min, max);
   }
 
   //make guess makes it all happen
@@ -44,18 +44,28 @@ async function numberGame() {
     //computer receives input and then checks that input to decide if it guessed the right number or needs to keep going forward
     checkYesOrNo(inputYorN);
     if (checkYesOrNo(inputYorN) === true) {
-    //checkYesOrNo returns true if the user answers "no you did not guess the number correctly," 
+      //checkYesOrNo returns true if the user answers "no you did not guess the number correctly," and ends the program if the answer is "yes that's my number"
       let inputHorL = await ask("Is your number higher or lower?\n>_ ");
-    //the computer will then make a new number, updating the min and max so that next time around it will make an appropriate guess      
-      makeNewNumber(inputHorL, guess);
+      inputHorL = inputHorL.charAt(0).toUpperCase(0);
+     //this logic tree will bypass someone who lies about wether their number is higher or lower, and keep guessing as though they did not lie. 
+      if (inputHorL === "H" && secretNumber < guess) {
+        console.log("That's just like, your opinion man.");
+        inputHorL = "L";
+        makeNewNumber(inputHorL, guess);
+      } else if (inputHorL === "L" && secretnumber > guess) {
+        console.log("That's just like, your opinion man.");
+        inputHorL = "H";
+        makeNewNumber(inputHorL, guess);
+      } else {
+        makeNewNumber(inputHorL, guess);
+      }
+      //the computer will then make a new number, updating the min and max within the function so that next time around it will make an appropriate guess
     }
   }
 
-  //need to come back to finish one of several cheat detectors i plan to make, having trouble with passing guess and secretNumber to this function without it being read twice, also usually as undefined will come back to it...
   function checkYesOrNo(inputYorN) {
     input = inputYorN.charAt(0).toUpperCase(0);
-     //one problem I have is that line 58 isn't actually checking against the guess and secretNumber... if you wrote "you're a jerk!" the computer would say.. Hurray! =p
-      if (input === "Y") {
+    if (input === "Y") {
       return console.log("Hurray we did it!!!")(process.exit());
     } else if (input === "N") {
       return true;
@@ -64,10 +74,10 @@ async function numberGame() {
 
   //here is the function that updates the minimum and maximum allowing future guesses to appropriate
   function makeNewNumber(inputHorL, guess) {
-    input = inputHorL.charAt(0).toUpperCase(0);
-    if (input === "H") {
+    // input = inputHorL.charAt(0).toUpperCase(0);
+    if (inputHorL === "H") {
       min = guess;
-    } else if (input !== "H") {
+    } else if (inputHorL !== "H") {
       max = guess;
     }
   }
@@ -78,23 +88,19 @@ async function selectGameType() {
   console.log(
     "Let's play GUESS THE NUMBER! it's my favorite game...\nactually, it's the only game I know.\nOne of us will pick a number, and the other one will try to guess it."
   );
-  console.log(
-    "If you would like me to guess your number, say something like,\n'You guess my number!' or 'You go first!'... say anything that starts with a Y actually..."
-  );
-  console.log(
-    "And if you would like to guess my number say something like,\n'I'll guess your number!' or 'I'll go first!'... say anything that starts with an 'I'..."
-  );
-  let selection = await ask("...Sorry I'm actually quite dumb\n>_ ");
+  console.log("Game One: You will pick a number, and I will try to guess it.");
+  console.log("Game Two: I will pick a number, and you will try to guess it.");
+  let selection = await ask("Would you like to play game one or two?\n>_ ");
   selectionClean = selection.charAt(0).toUpperCase(0);
 
-  if (selectionClean === "Y") {
+  if (selectionClean === "1" || selectionClean === "O") {
     numberGame();
-  } else if (selectionClean === "I") {
+  } else if (selectionClean === "2" || selectionClean === "T") {
     reverseGame();
   }
 }
 
-//this is the reverse game! 
+//this is the reverse game!
 async function reverseGame() {
   console.log(
     "OK! we'll play the game where I pick a secret number and YOU try to guess it...\nbut first, I need you to tell me the numbers I am choosing between, and that you will be guessing between..."
@@ -121,7 +127,7 @@ async function reverseGame() {
       nextGuess = await ask("What is your next guess?\n>_ ");
       playerGuess = parseInt(nextGuess);
     } else if (playerGuess < secretNumberComp) {
-      console.log("Sorry my number is higher than your guess..");
+      console.log("Sorry my number is higher than your guess...");
       nextGuess = await ask("What is your next guess?\n>_ ");
       playerGuess = parseInt(nextGuess);
     } else if (playerGuess === secretNumberComp) {
@@ -136,4 +142,3 @@ function randomInteger(min, max) {
   let range = max - min + 1;
   return min + Math.floor(Math.random() * range) + min;
 }
-
